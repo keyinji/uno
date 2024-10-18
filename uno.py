fa9568e41e918eaa7d5486452bbc12393b0c7d82
Ke Yin Ji <ke.ji@dawsoncollege.qc.ca>
2024-10-18 09:42:50 -0400

    ok

--- uno.py
+++ uno.py
@@ -20,20 +20,27 @@ def start_game(deck):
 
 def main_loop(p1, p2, face_up, deck):
     current = 0
-    print(f"It is the player {current + 1}'s turn")
-    print(f"The face up card is {face_up}")
-    print(f"Player {current+1}'s hand is {p1}")
+    while len(p1) > 0 and len(p2):
+        print(f"It is the player {current + 1}'s turn")
+        print(f"The face up card is {face_up}")
+        print(f"Player {current+1}'s hand is {p1}")
 
-    # Pick or play a card
-    pick_option = int(input("Choose 1 to play and 2 to draw a card"))
-    if pick_option == 1: #user wants to play
-        index = int(input("What card do you want to play (starting at 1)"))
-        index -= 1
-        if p1[index][0] == face_up[0] or p1[index][1] == face_up[1]:
-            card_to_play = p1.pop(index)
-
-
-    if pick_option == 2:
-        p1.append(deck.pop(0))
+        # Pick or play a card
+        pick_option = int(input("Choose 1 to play and 2 to draw a card: "))
+        if pick_option == 1: #user wants to play
+            index = int(input("What card do you want to play (starting at 1): ")) - 1
+            if p1[index][0] == face_up[0] or p1[index][1] == face_up[1]:
+                face_up = p1.pop(index)
 
 
+        else:
+            p1.append(deck.pop(0))
+        p1, p2 = p2, p1
+        current = (current + 1) % 2
+
+
+###
+# +2 
+# make one change to the code, as in, add one feature
+# black cards 
+# 4 players 
